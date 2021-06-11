fb1742ec62e62b2f1da712cdcc89eaf92c1c79b2
junhyeok <pjunh1117@gmail.com>
2021-06-11 21:08:05 +0900

    cv 파라미터 값을 100으로 수정  -> 속도는 느리지만 99퍼의 정확률이 나옴

--- SVM.py
+++ SVM.py
@@ -5,8 +5,8 @@ from sklearn.model_selection import cross_val_score
 from sklearn.datasets import load_diabetes
 
 diabetse = datasets.load_diabetes()
-s = svm.SVC(gamma = 0.1)
-accuracies = cross_val_score(s, digit.data, digit.target, cv = 10)#cv = k, k가 크면 신뢰도는 향상되지만 실행시간이 증가
+s = svm.SVC(gamma = 0.001)
+accuracies = cross_val_score(s, digit.data, digit.target, cv = 100)#cv = k, k가 크면 신뢰도는 향상되지만 실행시간이 증가
 
 print(accuracies)
 print("평균 정확률:", accuracies.mean()*100,"%")
