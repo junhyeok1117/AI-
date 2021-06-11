51687065f97b5e9c556c7a6f11ef0d6ff95f986b
junhyeok <pjunh1117@gmail.com>
2021-06-11 21:05:34 +0900

    svm 기법사용

---
+++ SVM.py
@@ -0,0 +1,13 @@
+#sklearn_diabetes_dataset_SVM
+from sklearn import datasets
+from sklearn import svm
+from sklearn.model_selection import cross_val_score
+from sklearn.datasets import load_diabetes
+
+diabetse = datasets.load_diabetes()
+s = svm.SVC(gamma = 0.001)
+accuracies = cross_val_score(s, digit.data, digit.target, cv = 10)#cv = k, k가 크면 신뢰도는 향상되지만 실행시간이 증가
+
+print(accuracies)
+print("평균 정확률:", accuracies.mean()*100,"%")
+
