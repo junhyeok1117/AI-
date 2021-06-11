c1cb37abd98f866b7a30c68c3be06665d8429bd8
junhyeok <pjunh1117@gmail.com>
2021-06-11 21:06:08 +0900

    gamma 파라미터 값 수정

--- SVM.py
+++ SVM.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import cross_val_score
 from sklearn.datasets import load_diabetes
 
 diabetse = datasets.load_diabetes()
-s = svm.SVC(gamma = 0.001)
+s = svm.SVC(gamma = 0.1)
 accuracies = cross_val_score(s, digit.data, digit.target, cv = 10)#cv = k, k가 크면 신뢰도는 향상되지만 실행시간이 증가
 
 print(accuracies)
